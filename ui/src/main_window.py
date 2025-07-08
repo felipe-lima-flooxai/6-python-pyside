2073d72d7ef4e03ea1f3b487c395713c439ddde9
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-07-08 12:52:36 -0300

    feat: eventFilter in Pyside6

--- ui/src/main_window.py
+++ ui/src/main_window.py
@@ -1,5 +1,8 @@
 from window import Ui_MainWindow
+from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtCore import QObject, QEvent
+from typing import cast
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -8,9 +11,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.buttonEnviar.clicked.connect(self.changeLabelResult)
 
+        self.lineName.installEventFilter(self)
+
     def changeLabelResult(self):
         text = self.lineName.text()
         self.labelResult.setText(text)
+
+    def eventFilter(self, watched: QObject, event: QEvent):
+        if event.type() == QEvent.Type.KeyPress:
+            event = cast(QKeyEvent, event)
+            text = self.lineName.text()
+            self.labelResult.setText(text + event.text())
+            return super().eventFilter(watched, event)
 
 if __name__ == "__main__":
     app = QApplication()
